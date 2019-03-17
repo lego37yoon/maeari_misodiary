b2f0fbf000248ac8595832e0282520f3ae88baaf
윤정민(종이상자) <lego37yoon@outlook.com>
2019-03-17 17:48:50 +0900

    20190317 1.1 Beta 03 Build 06_2  - 개인정보 취급방침 및 그 외 파싱을 위한 작업

--- app/src/main/java/pw/pbdiary/maeari/misodiary/AgreementActivity.java
+++ app/src/main/java/pw/pbdiary/maeari/misodiary/AgreementActivity.java
@@ -2,7 +2,16 @@ package pw.pbdiary.maeari.misodiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class AgreementActivity extends AppCompatActivity {
 
@@ -10,5 +19,43 @@ public class AgreementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+    }
+    private class PageTaskToS extends AsyncTask<Void,Void,Void> {
+        private Elements elementone;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                Document doc = Jsoup.connect("https://www.misodiary.net/member/register").get();
+                elementone = doc.select("div[class=policy]").eq(0);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView pv = findViewById(R.id.policy_view);
+            pv.setText(Html.fromHtml(elementone.html()));
+        }
+    }
+    private class PageTaskPP extends AsyncTask<Void,Void,Void> {
+        private Elements elementtwo;
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try{
+                Document doc = Jsoup.connect("https://www.misodiary.net/member/register").get();
+                elementtwo = doc.select("div[class=policy]").eq(1);
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            TextView pv = findViewById(R.id.policy_view);
+            pv.setText(Html.fromHtml(elementtwo.html()));
+        }
     }
 }
