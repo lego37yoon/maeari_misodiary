024aee36d5ff53d50ffa544bc5bd34e20e67f10b
윤정민(종이상자) <lego37yoon@outlook.com>
2019-03-17 13:56:47 +0900

    20190317 1.1 Beta 03 Build 05-3  - SettingsFragment 생성: 기존 설정창 폐기

---
+++ app/src/main/java/pw/pbdiary/maeari/misodiary/SettingFragment.java
@@ -0,0 +1,8 @@
+package pw.pbdiary.maeari.misodiary;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceFragment;
+
+public class SettingFragment extends PreferenceFragment {
+    SharedPreferences prefs;
+}
