a4d674811ceafbabdd4cba48be72f3b080b175ea
윤정민(종이상자) <lego37yoon@outlook.com>
2019-03-17 22:54:26 +0900

    20190317 1.1 Beta 03 Build 06  - 설정 항목 완성: 적용을 앞두고 있는 상황  - 약관 동의 부분 테스트만 남기고 완성

--- app/src/main/java/pw/pbdiary/maeari/misodiary/AgreementActivity.java
+++ app/src/main/java/pw/pbdiary/maeari/misodiary/AgreementActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 public class AgreementActivity extends AppCompatActivity {
 
@@ -19,11 +23,39 @@ public class AgreementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+        Chip mfmppchip = (Chip) findViewById(R.id.mfmpp);
+        mfmppchip.setChecked(true);
+        new PageTaskMPP(AgreementActivity.this).execute();
+        mfmppchip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new PageTaskMPP(AgreementActivity.this).execute();
+            }
+        });
+        Chip mtosppchip = (Chip) findViewById(R.id.mtos);
+        mfmppchip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new PageTaskToS(AgreementActivity.this).execute();
+            }
+        });
+        Chip mppchip = (Chip) findViewById(R.id.mpp);
+        mfmppchip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new PageTaskPP(AgreementActivity.this).execute();
+            }
+        });
     }
-    private class PageTaskToS extends AsyncTask<Void,Void,Void> {
+    private static class PageTaskToS extends AsyncTask<Void,Void,String> {
         private Elements elementone;
+        private WeakReference<AgreementActivity> activityReference;
+
+        PageTaskToS(AgreementActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(Void... params) {
             try{
                 Document doc = Jsoup.connect("https://www.misodiary.net/member/register").get();
                 elementone = doc.select("div[class=policy]").eq(0);
@@ -34,15 +66,22 @@ public class AgreementActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            TextView pv = findViewById(R.id.policy_view);
+        protected void onPostExecute(String result) {
+            AgreementActivity activity = activityReference.get();
+            if (activity == null || activity.isFinishing()) return;
+            TextView pv = activity.findViewById(R.id.policy_view);
             pv.setText(Html.fromHtml(elementone.html()));
         }
     }
-    private class PageTaskPP extends AsyncTask<Void,Void,Void> {
+    private static class PageTaskPP extends AsyncTask<Void,Void,String> {
         private Elements elementtwo;
+        private WeakReference<AgreementActivity> activityReference;
+
+        PageTaskPP(AgreementActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected String doInBackground(Void... params) {
             try{
                 Document doc = Jsoup.connect("https://www.misodiary.net/member/register").get();
                 elementtwo = doc.select("div[class=policy]").eq(1);
@@ -53,9 +92,36 @@ public class AgreementActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            TextView pv = findViewById(R.id.policy_view);
+        protected void onPostExecute(String result) {
+            AgreementActivity activity = activityReference.get();
+            if (activity == null || activity.isFinishing()) return;
+            TextView pv = activity.findViewById(R.id.policy_view);
             pv.setText(Html.fromHtml(elementtwo.html()));
+        }
+    }
+    private static class PageTaskMPP extends AsyncTask<Void,Void,String> {
+        private Document doc;
+        private WeakReference<AgreementActivity> activityReference;
+
+        PageTaskMPP(AgreementActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
+        @Override
+        protected String doInBackground(Void... params) {
+            try{
+                doc = Jsoup.connect("https://latios.pbdiary.pw/maeari/misodiary/privacypolicy.html").get();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            AgreementActivity activity = activityReference.get();
+            if (activity == null || activity.isFinishing()) return;
+            TextView pv = activity.findViewById(R.id.policy_view);
+            pv.setText(Html.fromHtml(doc.html()));
         }
     }
 }
--- app/src/main/java/pw/pbdiary/maeari/misodiary/SettingFragment.java
+++ app/src/main/java/pw/pbdiary/maeari/misodiary/SettingFragment.java
@@ -1,8 +1,15 @@
 package pw.pbdiary.maeari.misodiary;
 
 import android.content.SharedPreferences;
-import android.preference.PreferenceFragment;
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragment;
 
 public class SettingFragment extends PreferenceFragment {
     SharedPreferences prefs;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
 }
