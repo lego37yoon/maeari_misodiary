36c9d0893a8580200585942e234ccdec9f779f18
윤정민(종이상자) <lego37yoon@outlook.com>
2019-03-19 21:51:33 +0900

    20190319 1.1 Beta 03 Build 08_2  - 설정 화면 강제 종료되는 오류 수정  - 일부 테마가 잘못 설정되어 있는 오류 수정

--- app/src/main/java/pw/pbdiary/maeari/misodiary/SettingsActivity.java
+++ app/src/main/java/pw/pbdiary/maeari/misodiary/SettingsActivity.java
@@ -3,12 +3,18 @@ package pw.pbdiary.maeari.misodiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
         getSupportFragmentManager().beginTransaction().replace(R.id.settingLayout,new SettingFragment()).commit();
+    }
+
+    public void onSettingsBackPressed(View view) {
+        finish();
     }
 }
